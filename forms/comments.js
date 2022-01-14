4f4c366deb2c31e00f1822b81d0dfdee2113b6fe
SadiaNurNazifa <nazifanaz15@gmail.com>
2022-01-15 02:33:21 +0600

    4.1
    massive changes

--- forms/comments.js
+++ forms/comments.js
@@ -1,22 +1,22 @@
-function storecomment() {
+// function storecomment() {
 
-  var _name = document.getElementById("name").value;
-  var _email= document.getElementById("email").value;
-  var _post= document.getElementById("post").value;
+//   var _name = document.getElementById("name").value;
+//   var _email= document.getElementById("email").value;
+//   var _post= document.getElementById("post").value;
 
 
-   firebase.database().ref('User/' + _name).set({
-          name : _name,
-          email : _email,
-          comment : _post
-        }, function(error) {
-          if (error) {
-            alert('Error Occured');
-          } else {
-              alert("Sent");
-            console.log('Hoise');
+//    firebase.database().ref('User/' + _name).set({
+//           name : _name,
+//           email : _email,
+//           comment : _post
+//         }, function(error) {
+//           if (error) {
+//             alert('Error Occured');
+//           } else {
+//               alert("Sent");
+//             console.log('Hoise');
          
-          }
-        });
-}
+//           }
+//         });
+// }
 
